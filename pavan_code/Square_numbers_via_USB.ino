b353e035f3d504a5f6cbf08e8dabe197ca197928
Pavan <ee24b048@smail.iitm.ac.in>
2025-07-04 10:50:37 +0530

    feat: Added Square_numbers_via_USB.ino under pavan_code folder

---
+++ pavan_code/Square_numbers_via_USB.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+int n=0;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int u = n*n;
+  Serial.println(u);
+  n+=1;
+  delay(1000);
+
+}
