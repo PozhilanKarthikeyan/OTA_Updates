800512109712a9d9ba1774f6555220ba2697844f
shriharisj <shriharisj@users.noreply.github.com>
2025-07-07 12:39:22 +0530

    feat(ota): add ability to change Serial output based on OTA/USB upload, add comments

--- code/ota_first/ota_first.ino
+++ code/ota_first/ota_first.ino
@@ -1,40 +1,58 @@
 /*
 
+THIS IS AN EXAMPLE CODE ON FIBONACCI SERIES / SQUARE SERIES BASED ON OTA OR USB UPLOAD  
+
 DOCUMENTATION ON HOW TO UPLOAD THE CODE VIA OTA AFTER UPLOADING IT USING USB (ENTERPRISE NETWORKS):
 
 YOU CAN DIRECTLY USE UPLOAD BUTTON IF YOU ARE USING LOCAL NETWORK
-
 1) After you made your changes, go to Sketch > Export Compiled Binary on Arduino IDE or 
 build the project in PlatformIO to get a file like {file_name}.ino.bin
-
 2) In case of Arduino IDE you have to go inside build folder to find the required file. In PlatformIO find the bin file inside pio/build
-
 3) Locate espota.py using find ~ -name espota.py
-
 4) Run python3 {path_to_espota.py} -i {your_wifi_IP} -p 3232 -auth={your_OTA_Password} --file {path_to_the_bin_file}
-
 YOU CAN ALSO FIND THE VIDEO IN media
 
 */
 
+#include <Preferences.h>
 #include <ArduinoOTA.h>
 #include <WiFi.h>
 #include <esp_wpa2.h>
 
+// bool variables to check conditions
 bool isEnterprise = false;
 bool isLooped = false;
+bool isOTA = false;
+bool isSeriesTitlePrinted = false;
 
+// enterprise wifi details
 const String ENTERPRISE_WIFI[] = {
   "iitmwifi",
 };
 const int NO_ENTERPRISE_WIFI = sizeof(ENTERPRISE_WIFI)/sizeof(ENTERPRISE_WIFI[0]);
 
+// OTA host details
 const String OTA_HOSTNAME = "esp32test";
 const String OTA_PASSWORD = "12345678";
 
+// WiFi details
 String ssid;
 String password;
 String username;
+
+// for checking OTA or USB upload
+Preferences preferences;
+
+// THE BELOW VARIABLES ARE NOT REQUIRED FOR A BASIC OTA/USB UPLOAD
+// for looping in loop()
+unsigned long lastPrintTime = millis();
+
+// Square Series variables
+int square_n = 0;
+
+// Fiboncacci Series variables
+int fibo_a = 0;
+int fibo_b = 1;
 
 void setup() {
   
@@ -70,6 +88,7 @@ void setup() {
   ssid = Serial.readStringUntil('\n');
   ssid.trim(); // remove whitespace
 
+  // check for enterprise networks
   isEnterprise = false;
   for(int i = 0; i < NO_ENTERPRISE_WIFI; i++) {
     if (ssid == ENTERPRISE_WIFI[i]) {
@@ -78,12 +97,14 @@ void setup() {
     }
   }
 
+  // proceed with 2 actions based on isEnterprise or not
   if (!isEnterprise) {
     Serial.println("Enter Password: ");
     while (!Serial.available()) delay(10);
     password = Serial.readStringUntil('\n');
     password.trim(); // remove whitespace
 
+    // connect to WiFi
     Serial.print("Connecting to WiFi");
     WiFi.begin(ssid.c_str(), password.c_str()); // convert to c string before sending connection
   }
@@ -105,9 +126,11 @@ void setup() {
     esp_wifi_sta_wpa2_ent_set_password((uint8_t*)password.c_str(), password.length()); 
     esp_wifi_sta_wpa2_ent_enable();
 
+    // connect to WiFi
     WiFi.begin(ssid.c_str()); // use c strings
   }
 
+  // restrict the no of connection attempts
   int attempts = 0;
   while (WiFi.status() != WL_CONNECTED && attempts < 40){
     delay(500);
@@ -117,12 +140,31 @@ void setup() {
 
   Serial.println();
 
+  // configure OTA and print success message
   if (WiFi.status() == WL_CONNECTED) {
     Serial.print("Connected to WIFI with SSID ");
     Serial.print(ssid);
     Serial.print(" and with IP address ");
     Serial.print(WiFi.localIP());
-    Serial.println(". This code is updated via USB upload.");
+    Serial.println();
+    
+    // get the isOTA variable state (upload is a folder where isOTA is a variable in preferences)
+    preferences.begin("upload", true);
+    isOTA = preferences.getBool("isOTA", false);
+    preferences.end();
+
+    // print upload via OTA or USB
+    if(isOTA) {
+      Serial.println("This code was uploaded via OTA.");
+    }
+    else {
+      Serial.println("This code was uploaded via USB.");
+    }
+
+    // reset preferences
+    preferences.begin("upload", false);
+    preferences.putBool("isOTA", false); 
+    preferences.end();
 
     // configure OTA
     ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
@@ -130,6 +172,12 @@ void setup() {
     
     // OTA event handlers
     ArduinoOTA.onStart([]() {
+      
+      // set isOTA upload to true
+      preferences.begin("upload", false);
+      preferences.putBool("isOTA", true);
+      preferences.end();
+
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH) {
         type = "sketch";
@@ -177,14 +225,50 @@ void loop() {
  if (WiFi.status() == WL_CONNECTED) {
   ArduinoOTA.handle();
 
-  // main code
+  // main code: generate fibonacci series for OTA upload and square series for USB upload
+  // SKIP IF YOU DON'T WANT THIS FUNCTIONALITY
 
-  if (!isLooped) {
-    Serial.println("This code is uploaded via OTA");
-    Serial.println("test35");
-    isLooped = true;
+  unsigned long currentTime = millis();
 
+  if (currentTime - lastPrintTime > 1000) {
+    
+    // update lastPrintTime
+    lastPrintTime = currentTime;
+
+    if (isOTA) {
+
+      if (!isSeriesTitlePrinted) {
+        Serial.println("Fibonacci Series:");
+        isSeriesTitlePrinted = true;
+
+        // print the initial 2 terms
+        Serial.println(fibo_a);
+        Serial.println(fibo_b);
+      }
+
+      // main Fibonacci series logic
+      int fibo_c = fibo_a + fibo_b;
+      Serial.println(fibo_c);
+      fibo_a = fibo_b;
+      fibo_b = fibo_c;
+
+    }
+
+    else {
+
+      if (!isSeriesTitlePrinted) {
+        Serial.println("Square Series:");
+        isSeriesTitlePrinted = true;
+      }
+
+      // main Square series logic
+      Serial.println(square_n * square_n);
+      square_n++;
+    
+    }
+    
   }
+
  }
   
 }
